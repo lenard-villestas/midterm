7a3e34df27892280a35c08aaa164a2aa67637c41
Lenard Villestas <lenard.villestas@outlook.com>
2022-06-30 20:13:06 -0600

    adds more line to learn react

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React Right Now
+          Learn React Right Now now now ayo
         </a>
       </header>
     </div>
