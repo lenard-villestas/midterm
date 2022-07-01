182c333e6410e728973c5d4905aefd37f09b586d
Lenard Villestas <lenard.villestas@outlook.com>
2022-06-30 20:55:45 -0600

    adds yesir

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React Right Now
+          Learn React Right Now Ye
         </a>
       </header>
     </div>
